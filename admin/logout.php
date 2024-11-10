1f7b8dc1b388162f2fe36bce957d9ebbce21b515
Hữu Nghĩa <107995002+Huunghiapx@users.noreply.github.com>
2024-11-11 03:11:31 +0700

    phanquyen

--- admin/logout.php
+++ admin/logout.php
@@ -1,12 +1,16 @@
 <?php 
 
 session_start();
-if (isset($_GET['logout']) && $_GET['logout'] == 1 ) {
-    if(isset($_SESSION['admin_logged_in'])) {
-        unset($_SESSION['admin_logged_in']);
+if (isset($_GET['logout']) && $_GET['logout'] == 1) {
+    if (isset($_SESSION['logged_in']) && $_SESSION['role'] === 'admin') {
+        // Hủy session của admin
+        unset($_SESSION['logged_in']);
+        unset($_SESSION['role']);
         unset($_SESSION['admin_email']);
         unset($_SESSION['admin_name']);
-        header('location: login.php');
+        
+        // Chuyển hướng đến trang đăng nhập
+        header('location: ../login.php');
         exit;
     }
 }
